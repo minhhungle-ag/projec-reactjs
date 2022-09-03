ae559da755896d69283b397a2d713c7947c8d6e0
minhhungle <haless132@gmail.com>
2022-09-03 14:02:44 +0700

    update

--- src/featutes/home/components/BestSell.jsx
+++ src/featutes/home/components/BestSell.jsx
@@ -19,13 +19,15 @@ function BestSell(props) {
         </Typography>
 
         <Stack direction="row" flexWrap="wrap" sx={{ mx: -1 }}>
-          {productList.map((item, idx) => (
-            <Box sx={{ width: { xs: "100%", sm: 1 / 2, md: 1 / 4 } }}>
-              <Box sx={{ p: 1 }}>
-                <ProductCard product={item} />
+          {productList
+            .filter((item, idx) => idx < 4)
+            .map((item, idx) => (
+              <Box sx={{ width: { xs: "100%", sm: 1 / 2, md: 1 / 4 } }}>
+                <Box sx={{ p: 1 }}>
+                  <ProductCard product={item} />
+                </Box>
               </Box>
-            </Box>
-          ))}
+            ))}
         </Stack>
       </Container>
     </Box>
